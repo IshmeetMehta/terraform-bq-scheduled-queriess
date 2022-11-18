406185893ba5e0ee86d992c5577dfab31931c979
Ishmeet Mehta <ishmeetm@google.com>
2022-11-18 15:32:04 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,16 +49,15 @@ resource "google_project_iam_member" "bq-scheduled-query-sa-iam" {
   member     = "serviceAccount:${google_service_account.bq-scheduled-query-sa.email}"
 }
 
-# resource "google_project_iam_member" "permissions" {
-#   role   = "roles/iam.serviceAccountTokenCreator"
-#   project = data.google_project.project.project_id
-#   member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
-# }
+resource "google_project_iam_member" "permissions" {
+  role   = "roles/iam.serviceAccountTokenCreator"
+  project = data.google_project.project.project_id
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
+}
 
 # create a scheduled query
 resource "google_bigquery_data_transfer_config" "query_config" {
-# depends_on = [google_project_iam_member.permissions, google_project_iam_member.bq-scheduled-query-sa-iam]
-  depends_on = [google_project_iam_member.bq-scheduled-query-sa-iam]
+  depends_on = [google_project_iam_member.permissions, google_project_iam_member.bq-scheduled-query-sa-iam]
 
   display_name           = "my-query"
   location               = "US"
